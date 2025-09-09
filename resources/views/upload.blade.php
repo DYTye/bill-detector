133ad99487fa135161b3fa19206160a758b728f4
Aditya <adityaarrofi806@gmail.com>
2025-09-09 21:09:25 +0700

    initial commit

---
+++ resources/views/upload.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Upload</title>
+</head>
+<body>
+    <section class="title">
+        <h2>Select media to Upload</h2>
+
+        <div class="container">
+            <form action="#" method="POST" enctype="multipart/form-data">
+                <input type="file" name="recipt" accept="image/*" required>
+                <button type="submit">Upload</button>
+            </form>
+        </div>
+    </section>
+    </div>
+</body>
+</html>
